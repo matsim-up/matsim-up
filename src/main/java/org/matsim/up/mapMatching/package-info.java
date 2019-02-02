0d3a29364354b04fb05e014db09134845a4f2839
Johan W. Joubert <johan.joubert@up.ac.za>
2019-02-02 13:06:37 +0200

    Empty mapmatching folder

---
+++ src/main/java/org/matsim/up/mapMatching/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes and utilities specific to the MATSim team at the University of Pretoria.
+ * This map-matching algorithm initially started with the Masters dissertation of
+ * Mr Jaco-Ben Vosloo.
+ * 
+ * @see <a href=http://www.up.ac.za>Vosloo, J.B. (2018). <i>Development of a map-matching
+ * algorithm for dynamic sampling rate GPS signals to determine vehicle routes on a 
+ * MATSim network</i>. Masters dissertation, University of Pretoria, South Africa.</a>
+ * 
+ * @author jwjoubert
+ */
+package org.matsim.up.mapMatching;
