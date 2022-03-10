aeac78835575a836d2c57d19fa67e6efd3816258
JW Joubert <johan.joubert@up.ac.za>
2022-03-10 11:08:16 +0200

    Adding a compiling file
    This file is compiling but not functional. It is just to get the build to rerun again.

---
+++ src/main/java/org/matsim/up/utils/spatial/UpShapefiles.java
@@ -0,0 +1,23 @@
+package org.matsim.up.utils.spatial;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+
+public enum UpShapefiles {
+
+    CAPETOWN_FUNCTIONAL("CapeTown", "https://ie-repo.up.ac.za/data/shapefiles/-/blob/master/capeTown/zones/functional/CapeTown_Functional_SP2011_WGS84.shp");
+
+    private final String name;
+    private final String url;
+
+    UpShapefiles(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public URL getUrl() throws MalformedURLException {
+        return new URL( String.format(Locale.US, "%s", url) );
+    }
+
+}
